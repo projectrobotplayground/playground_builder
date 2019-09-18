de61e0feae573c6b3e1633ab6f9a397e83ccc636
Allison Thackston <allison@lyonthackston.com>
2019-09-17 23:15:08 -0700

    added command line options to gzthumbnails

--- thumbnail_gen/gzthumbnails.sh
+++ thumbnail_gen/gzthumbnails.sh
@@ -2,19 +2,54 @@
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 ASSETS="$(pwd)"
+OVERWRITE=false
+BG="black"
+
+usage()
+{
+    echo "usage: gzthumbnails.sh [[[-d dir ] [-o overwrite] [-b background]] | [-h]]"
+}
+
+while [ "$1" != "" ]; do
+    case $1 in
+        -d | --dir )            shift
+                                DIR=$1
+                                ;;
+        -o | --overwrite )      OVERWRITE=true
+                                ;;
+        -b | --background )     shift
+                                BG=$1
+                                ;;
+        -h | --help )           usage
+                                exit
+                                ;;
+        * )                     usage
+                                exit 1
+    esac
+    shift
+done
+
+if [ "$BG" = "black" ]; then
+  COLOR="#000000"
+elif [ "$BG" = "green" ]; then
+  COLOR="#00ff00"
+fi
 
 for dir in $ASSETS/*/
 do
   dir=${dir%*/}
   if [[ -f $ASSETS/${dir##*/}/model.sdf ]]; then
+    if [OVERWRITE]; then
+      rm -fr $ASSETS/${dir##*/}/thumbnails/
+    fi
     if [[ ! -d $ASSETS/${dir##*/}/thumbnails/ ]]; then
       echo "Creating thumbnail for ${dir##*/}"
       # generate thumbnails with solid bg
-      gzserver -s libModelPropShop.so $DIR/black.world --propshop-save "$ASSETS/${dir##*/}/thumbnails" --propshop-model "$ASSETS/${dir##*/}/model.sdf"
+      gzserver -s libModelPropShop.so $DIR/$BG.world --propshop-save "$ASSETS/${dir##*/}/thumbnails" --propshop-model "$ASSETS/${dir##*/}/model.sdf"
       for file in $ASSETS/${dir##*/}/thumbnails/*
       do
         # make bg transparent
-        color="#000000"
+        color=$COLOR
         convert $file -alpha off -bordercolor $color -border 1 \
           \( +clone -fuzz 5% -fill none -floodfill +0+0 $color \
             -alpha extract -geometry 200% -blur 0x0.5 \
